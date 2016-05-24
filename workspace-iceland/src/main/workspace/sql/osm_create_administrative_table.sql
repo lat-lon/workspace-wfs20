a830f6590f150ed3cf20e1c2f49b9cb49181bb57
Lena Rippolz <rippolz@lat-lon.de>
2016-05-24 15:26:34 +0200

    Changed user permissions and delete line break

--- workspace-iceland/src/main/workspace/sql/osm_create_administrative_table.sql
+++ workspace-iceland/src/main/workspace/sql/osm_create_administrative_table.sql
@@ -1,3 +1,2 @@
-create table osm_administrative as select osm_id, admin_level, name, place, way_
-area, way as geometry from planet_osm_polygon where boundary = 'administrative';
+create table osm_administrative as select osm_id, admin_level, name, place, way_area, way as geometry from planet_osm_polygon where boundary = 'administrative';
 grant select on osm_administrative to deegree ;
