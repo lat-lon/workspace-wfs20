513182bb702513f3453e8a06b8d6a01cd501ac51
Dirk Stenger <stenger@lat-lon.de>
2016-05-31 11:44:30 +0200

    Refactoring

--- workspace-iceland/src/main/workspace/sql/install-db.sh
+++ workspace-iceland/src/main/workspace/sql/install-db.sh
@@ -25,7 +25,7 @@ function importGN {
 
 function importOSM {
   osm2pgsql --keep-coastlines -s -d iceland ../data/iceland-latest.osm.pbf
-  psql -d iceland -f osm-import_to_epsg-4326.sql
+  psql -d iceland -f osm_import_to_epsg-4326.sql
   psql -d iceland -f osm_create_administrative_table.sql
   psql -d iceland -f osm_create_protected_area_table.sql
   psql -d iceland -f osm_drop_tables.sql
