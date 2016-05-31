6be4696afe6af4bf36ecb686a7f69bf0ca483723
Dirk Stenger <stenger@lat-lon.de>
2016-05-31 13:08:14 +0200

    Enhanced permissions of user deegree to access osm tables

--- workspace-iceland/src/main/workspace/sql/osm_create_administrative_table.sql
+++ workspace-iceland/src/main/workspace/sql/osm_create_administrative_table.sql
@@ -1,2 +1,2 @@
 create table osm_administrative as select osm_id, admin_level, name, place, way_area, way as geometry from planet_osm_polygon where boundary = 'administrative';
-grant select on osm_administrative to deegree ;
+grant all on osm_administrative to deegree;
--- workspace-iceland/src/main/workspace/sql/osm_create_protected_area_table.sql
+++ workspace-iceland/src/main/workspace/sql/osm_create_protected_area_table.sql
@@ -1,2 +1,2 @@
 create table osm_protected_area as select osm_id, boundary, leisure, name, tourism, way_area, way as geometry from planet_osm_polygon where boundary in ('protected_area', 'national_park');
-grant SELECT on osm_protected_area to deegree;
+grant all on osm_protected_area to deegree;
