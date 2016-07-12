8dfc43ef33153b6908eef50806c8b9ea7e4555d9
Lyn Elisa Goltz <goltz@lat-lon.de>
2016-07-12 20:58:37 +0200

    #3664 - import osm_timestamp; configured osm_timestamp

--- workspace-iceland/src/main/workspace/sql/osm_create_administrative_table.sql
+++ workspace-iceland/src/main/workspace/sql/osm_create_administrative_table.sql
@@ -1,4 +1,4 @@
-create table osm_administrative as select osm_id, admin_level, name, place, way_area, way as geometry from planet_osm_polygon where boundary = 'administrative';
+create table osm_administrative as select osm_id, admin_level, name, place, osm_timestamp, way_area, way as geometry from planet_osm_polygon where boundary = 'administrative';
 grant all on osm_administrative to deegree;
 
 create sequence osm_administrative_seq;
@@ -9,8 +9,3 @@ ALTER TABLE osm_administrative ADD COLUMN gml_identifier varchar;
 ALTER TABLE osm_administrative ADD COLUMN gml_name varchar;
 ALTER TABLE osm_administrative ADD COLUMN place_attr_nil boolean;
 ALTER TABLE osm_administrative ADD COLUMN place_attr_nilreason text;
-ALTER TABLE osm_administrative ADD COLUMN administrative_date date;
-ALTER TABLE osm_administrative ADD COLUMN administrative_datetime timestamp;
-
-UPDATE osm_administrative set administrative_date = cast(date '2018-07-01' - trunc( osm_id & 50 ) * '1 month'::interval as date);
-UPDATE osm_administrative set administrative_datetime = cast(timestamp '2018-07-01 20:03:50' - trunc( (osm_id & 100)) * '1 month'::interval as timestamp);
--- workspace-iceland/src/main/workspace/sql/osm_create_protected_area_table.sql
+++ workspace-iceland/src/main/workspace/sql/osm_create_protected_area_table.sql
@@ -1,4 +1,4 @@
-create table osm_protected_area as select osm_id, boundary, leisure, name, tourism, way_area, way as geometry from planet_osm_polygon where boundary in ('protected_area', 'national_park');
+create table osm_protected_area as select osm_id, boundary, leisure, name, tourism, osm_timestamp, way_area, way as geometry from planet_osm_polygon where boundary in ('protected_area', 'national_park');
 grant all on osm_protected_area to deegree;
 
 create sequence osm_protected_area_seq;
@@ -7,8 +7,3 @@ alter table osm_protected_area add column id integer default nextval('osm_protec
 ALTER TABLE osm_protected_area ADD COLUMN gml_description varchar;
 ALTER TABLE osm_protected_area ADD COLUMN gml_identifier varchar;
 ALTER TABLE osm_protected_area ADD COLUMN gml_name varchar;
-ALTER TABLE osm_protected_area ADD COLUMN protected_area_date date;
-ALTER TABLE osm_protected_area ADD COLUMN protected_area_datetime timestamp;
-
-UPDATE osm_protected_area set protected_area_date = cast(date '2016-07-01' - trunc( osm_id & 50 ) * '1 month'::interval as date);
-UPDATE osm_protected_area set protected_area_datetime = cast(timestamp '2016-07-01 20:03:50' - trunc( (osm_id & 100)) * '1 month'::interval as timestamp);
